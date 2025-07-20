6abc355c0ed43743160a42d594739b0de2b2d2e0
Teezaar <javadghafouri1999@gmail.com>
2025-07-20 19:33:30 +0330

    feat(api): implement OpenAPI schema and Swagger UI
    - Add drf-spectacular and Django REST Framework to installed apps
    - Configure REST_FRAMEWORK settings for OpenAPI schema
    - Update URLs to include API schema and Swagger views
    - Upgrade Django REST Framework version in requirements.txt

--- app/app/urls.py
+++ app/app/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # The YAML OpenAPI file will be available at this URL.
+    path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
+    # The Swagger UI will be available at this URL.
+    path(
+        'api/docs/', SpectacularSwaggerView.as_view(url_name='api-schema'), name='api-docs'
+        ),
+
 ]
